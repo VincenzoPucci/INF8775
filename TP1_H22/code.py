b27d7cc9502383bc62bc287aaa40488bb2d36aa3
Matthieu_Bossert <matthieu.bossert@epfl.ch>
2022-02-09 13:34:47 -0500

    added performance files added plot_performance function to find threshold

--- TP1_H22/code.py
+++ TP1_H22/code.py
@@ -1,5 +1,6 @@
 from collections import deque
-import os,glob
+import time
+import matplotlib.pyplot as plt
 
 
 def make_critical_pt(list_building):
@@ -72,33 +73,76 @@ def divide(list_buildings, threshold = 1):
         sol2 = divide(list_buildings[len(list_buildings)//2:])
         return merge(sol1, sol2)
 
-def print_results(result, option = "-p"):
+def print_results(result, option = "-p", time = None):
     if option == "-p":
         for i in result:
             print(f"{i[0]} {i[1]}")
+    elif option == "-t":
+        print(time)
+    else:
+        print(f"Illegal argument {option}. Correct arguments are -p or -t")
 
 
-def solve_skyline(path):
+def solve_skyline(path, option = "-p"):
     with open(path, 'r') as pointFile:
         pointList = []
         next(pointFile)
         for x in pointFile:
             a, b, c = (int(x) for x in x.split())
             pointList.append((a, b, c))
+    
+    time_init = time.perf_counter()
     result_naive = naive(pointList)
-    print_results(result_naive, "-p")
+    time_fin = time.perf_counter()
+    time_naive = time_fin - time_init
+    #print_results(result_naive, option, time_naive)
+
+    time_init = time.perf_counter()
     result_divide = divide(pointList)
-    #print(result_divide)
+    time_fin = time.perf_counter()
+    time_divide = time_fin - time_init
+    #print_results(result_divide, option, time_divide)
+
+    return time_naive, time_divide
 
     #check if both algorithms return same answer
     #print(result_naive == result_divide)
     #print([result_divide.index(item) for item in result_divide if item not in result_naive])
 
+def perf_threshold(list_size_sample, number_sample = 1):
+    list_time_naive = []
+    list_time_divide = []
+
+    for size_sample in list_size_sample:
+        time_naive = 0
+        time_divide = 0
+        for n in range(number_sample):
+            t1, t2 = solve_skyline(f"threshold/N{size_sample}_{n}", "-t")
+            time_naive += t1
+            time_divide += t2
+        #take the average time
+        time_naive = time_naive / number_sample
+        time_divide = time_divide / number_sample
+        list_time_naive.append(time_naive)
+        list_time_divide.append(time_divide)
+
+    return list_time_naive, list_time_divide
+
+def plot_performance():
+    list_size_sample = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500]
+    list_time_naive, list_time_divide = perf_threshold(list_size_sample, 10)
+    plt.loglog(list_size_sample, list_time_naive, label = "brute_force")
+    plt.loglog(list_size_sample, list_time_divide, label = "divide_and_conquer")
+    plt.legend()
+    plt.xlabel("size_sample")
+    plt.ylabel("time")
+    plt.title("performance in function of sample size")
+    plt.savefig("performance.pdf", format = "pdf")
+    plt.show()
 
 if __name__ == '__main__':
 
-    solve_skyline("test/N1000_0")
-
+    plot_performance()
 
     # sizes = {"1000","5000","10000","50000","100000","500000"}
     # n_samples = 5
