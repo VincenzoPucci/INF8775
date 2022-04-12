e546b04d782019ce6f6b15cd775bf05a0e32815b
Vincenzo Pucci <vincenzo.pucci-barbeau@polymtl.ca>
2022-04-12 14:44:30 -0400

    Can read the file now

--- TP3_H22/tp.py
+++ TP3_H22/tp.py
@@ -1,15 +1,36 @@
+t = 0
+k = 0
+A = 0
+nbAt = []
+H = []
+G = []
+
+
 def getData(path):
     with open(path, 'r') as graphFile:
-        pointList = []
-        # next(graphFile)
-        for x in graphFile:
-            a, b, c = (int(x) for x in x.split())
-            pointList.append((a, b, c))
-    return 1
+        max = 0
+        for idx, x in enumerate(graphFile):
+            if x != "\n":
+                if idx == 0:
+                    t, k, A = (int(x) for x in x.split())
+                    max = 4 + k
+
+                elif idx == 2:
+                    nbAt = list(int(x) for x in x.split())
+
+                elif idx >= 4 and idx <= max:
+                    line1 = list(int(x) for x in x.split())
+                    H.append(line1)
+
+                elif idx >= max + 1:
+                    line = list(int(x) for x in x.split())
+                    G.append(line)
 
 
 def main():
     getData("N5_K3_0")
+    print(H)
+    print(G)
 
 
 if __name__ == "__main__":
