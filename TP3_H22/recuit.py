d69eaf0ffe30669a441af0b8ce1121eaaa6b8e52
Matthieu_Bossert <matthieu.bossert@epfl.ch>
2022-04-20 22:03:01 -0400

    nearly final code

--- TP3_H22/recuit.py
+++ TP3_H22/recuit.py
@@ -3,16 +3,16 @@ from utils import compute_energy, print_sol
 import random
 import math
 
-
 def recuit(solution, H, G, argument):
     best_score = compute_energy(solution, H, G)
     temperature = 10
-    runtime = 180  # in seconds
+    runtime = 180 #in seconds
     lam = 0.99
     energy = best_score
     temp_time = time()
     while True:
-        neighbour, energy_neighbour = find_neighbours(solution, energy, H, G)
+        neighbour = find_neighbours(solution)
+        energy_neighbour = compute_energy(neighbour, H, G)
         if energy_neighbour < energy:
             solution = neighbour
             energy = energy_neighbour
@@ -30,20 +30,21 @@ def recuit(solution, H, G, argument):
         if energy < best_score:
             best_score = energy
             if argument:
-                print_sol(solution)
+                print_sol(solution, flush=True)
             else:
                 print(energy, flush=True)
+        
+        
 
 
-def find_neighbours(sol, energy_sol, H, G):
+def find_neighbours(sol):
     n1 = random.randint(0, len(sol)-1)
     n2 = random.randint(0, len(sol)-1)
-    if n1 != n2:  # not interesting to swap a node with itself
+    if n1 != n2: #not interesting to swap a node with itself
         neighbour = sol.copy()
-        # we swap two nodes
+        #we swap two nodes
         neighbour[n1] = sol[n2]
         neighbour[n2] = sol[n1]
-        energy_neighbour = compute_energy(neighbour, H, G)
-        return neighbour, energy_neighbour
+        return neighbour
     else:
-        return sol, energy_sol
+        return sol
